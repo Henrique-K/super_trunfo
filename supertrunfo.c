1f5f169c15a50956a8104bd47b06ca9404f1c10f
Henrique-K <henriqueklafkebedinm@gmail.com>
2025-10-19 14:47:51 +0000

    0.2

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,48 +1,86 @@
 #include <stdio.h>
-
 int main(){
 
-    char estado1 = 'X'; //: Uma letra de 'A' a 'H' (representando um dos oito estados). Tipo: char
-    char codigo_da_Carta1 = '0'; //: A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)
-    char nome_da_cidade1[31] = "Nao escolhido"; //: O nome da cidade. Tipo: char[] (string)
-    int populacao1 = 0; //: O número de habitantes da cidade. Tipo: int
-    float area1 = 0; //(em km²): A área da cidade em quilômetros quadrados. Tipo: float
-    float Pib1 = 0; //: O Produto Interno Bruto da cidade. Tipo: float
-    int numero_de_pontos_turistico1 = 0; //: A quantidade de pontos turísticos na cidade. Tipo: int
-   /*
-    printf ("Digite uma letra de 'A' a 'H' para representar o estado:");
-    scanf ("%c", &estado1);
+    // Variaveis da primeira carta
+    char Estado_0 = 'Y';
+    char Código_da_Carta_0 = '0';
+    char Nome_da_Cidade_0[30] = "ZZZ"; 
+    int População_0 = 0;
+    float Área_0 = 0.01; 
+    float PIB_0 = 0.01;
+    int Número_de_Pontos_Turísticos_0 = 0;
+
+    // variaveis da segunda carta
+    char Estado_1 = 'Y';
+    char Código_da_Carta_1 = '0';
+    char Nome_da_Cidade_1[30] = "ZZZ"; 
+    int População_1 = 0;
+    float Área_1 = 0.01; 
+    float PIB_1 = 0.01;
+    int Número_de_Pontos_Turísticos_1 = 0;
+   
+    //coleta de informacoes da primeira carta
+    //na coleta de informaçoes o scanf no terminal nao finalizava no enter quando o "\n" estava no final do printf.
+    printf("\n________________________________________________________________________________________\n"); 
+    printf("\nCarta 1:\n");
+    printf("\nDígite uma letra de 'A' a 'H' para o estado da cidade:");
+    scanf(" %c",&Estado_0);
+    printf("\nDígite um número de 1 a 4 para o código da carta:");
+    scanf(" %c",&Código_da_Carta_0);
+    printf("\nDígite o nome da cidade:");
+    scanf(" %s",&Nome_da_Cidade_0);
+    printf("\nDígite a população da cidade:");
+    scanf(" %d",&População_0); 
+    printf("\nDígite a área da cidade:");
+    scanf(" %f",&Área_0);
+    printf("\nDígite o PIB da cidade:");
+    scanf(" %f",&PIB_0);
+    printf("\nDígite o número de pontos turísticos da cidade:");
+    scanf(" %d",&Número_de_Pontos_Turísticos_0);
     
-    printf ("Digite um numero de '1' a '4':");
-    scanf ("%c", &codigo_da_Carta1);    
-    scanf ("%c", &codigo_da_Carta1);
+    //mostragem de informacoes da primeira carta
+    printf("\n________________________________________________________________________________________\n\n");
+    printf("Carta 1:\n");
+    printf("Estado: %c \n", Estado_0);
+    printf("Código: %c0%c\n", Estado_0,Código_da_Carta_0);
+    printf("Nome da cidade: %s\n", Nome_da_Cidade_0);
+    printf("População: %d\n",População_0);
+    printf("Área: %.2f\n",Área_0);
+    printf("PIB: %.2f\n",PIB_0);
+    printf("Pontos turísticos: %d\n",Número_de_Pontos_Turísticos_0);
 
-    printf ("Digite a populacao da cidade:");
-    scanf ("%i", &populacao1);
+    //coleta de informacoes da segunda carta
+    //na coleta de informaçoes o scanf no terminal nao finalizava no enter quando o "\n" estava no final do printf.
+    printf("\n________________________________________________________________________________________\n");
+    printf("\nCarta 2:\n");
+    printf("\nDígite uma letra de 'A' a 'H' para o estado da cidade:");
+    scanf(" %c",&Estado_1);
+    printf("\nDígite um número de 1 a 4 para o código da carta:");
+    scanf(" %c",&Código_da_Carta_1);
+    printf("\nDígite o nome da cidade:");
+    scanf(" %s",&Nome_da_Cidade_1);
+    printf("\nDígite a população da cidade:");
+    scanf(" %d",&População_1);
+    printf("\nDígite a área da cidade:");
+    scanf(" %f",&Área_1);
+    printf("\nDígite o PIB da cidade:");
+    scanf(" %f",&PIB_1);
+    printf("\nDígite o número de pontos turísticos da cidade:");
+    scanf(" %d",&Número_de_Pontos_Turísticos_1);
     
-    printf ("Digite a area da cidade em Km²:");
-    scanf ("%f", &area1);
+    //mostragem de informacoes da segunda carta
+    printf("\n________________________________________________________________________________________\n\n");
+    printf("Carta 2:\n");
+    printf("Estado: %c \n", Estado_1);
+    printf("Código: %c0%c\n", Estado_1,Código_da_Carta_1);
+    printf("Nome da cidade: %s\n", Nome_da_Cidade_1);
+    printf("População: %d\n",População_1);
+    printf("Área: %.2f\n",Área_1);
+    printf("PIB: %.2f\n",PIB_1);
+    printf("Pontos turísticos: %d\n",Número_de_Pontos_Turísticos_1);
+    
 
-    printf ("Digite o PIB da cidade:");
-    scanf ("%f", &Pib1);
 
-    printf("Digite o numero de pontos turisticos:");
-    scanf ("%i", &numero_de_pontos_turistico1);
-
-    printf ("Digite o nome da cidade:");
-    scanf ("%s", &nome_da_cidade1);
-    */
-    printf("\n\n");
-    printf("Carta 1: \n");
-    printf("Estado: %c \n",&estado1);
-    printf("Codigo: %c0%c \n",&estado1, &codigo_da_Carta1);
-    printf("Nome da Cidade: %s \n", &nome_da_cidade1);
-    printf("Populacao: %i \n", &populacao1);
-    printf("Area: %f km² \n", &area1);
-    printf("PIB: %f reais \n", &Pib1);
-    printf("Numero de Pontos Turisticos: %i \n", &numero_de_pontos_turistico1);
-
-    int end = 2;
-    scanf("%i", &end, "/n");
+    printf("\n_______________________________________________________________________________________\n");
     return 0;
 }
